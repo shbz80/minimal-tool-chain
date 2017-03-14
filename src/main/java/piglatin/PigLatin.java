b0a0ae06422daf15f3a111afe1e5d863f5319702
Oliveira Rui <rufoliveira@gmail.com>
2017-03-14 15:21:57 +0100

    Added verbose flag support. When it is activated the user will be able to see the translation process.

--- src/main/java/piglatin/PigLatin.java
+++ src/main/java/piglatin/PigLatin.java
@@ -16,16 +16,12 @@ import java.io.FileReader;
 
 public class PigLatin {
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_CYAN = "\u001B[36m";
 
 	public static void main(String[] args) {    	
 
@@ -39,6 +35,15 @@ public class PigLatin {
 
             }
 
+            if ( args[i].equals("-verbose") ){
+
+                // If there is a verbose flag, explain the translation process in detail.          
+                readSentenceFromCommandLine(true);
+                return;
+
+            }
+
+
         }
 
         if ( args.length > 0 ){
@@ -50,15 +55,46 @@ public class PigLatin {
 
         }
 
-    	
+    	readSentenceFromCommandLine(false);
+        
+
+    }
+
+    private static void readSentenceFromCommandLine(boolean verbose){
+
         System.out.println("Type the sentence to be piggified:");
        
         Scanner sc = new Scanner(System.in);
         String sentence = sc.nextLine();
         String newSentence = PigNumberClass.replacePosIntInSentenceWithWords(sentence);
+
+        if (verbose){
+            explainingPrint(sentence);
+        }
+        
         String piggified_sentence = PigLatinConversions.pigiffySentence(newSentence);
         
         System.out.println("The piggified version of your input is:\n" + piggified_sentence);
+
+    }
+
+    private static void explainingPrint(String sentence){
+
+        String newSentence = PigNumberClass.replacePosIntInSentenceWithWords(sentence);
+        String piggified_sentence = PigLatinConversions.pigiffySentence(newSentence);
+
+        String[] inputTokens = newSentence.split(" ");
+        String[] pigTokens = piggified_sentence.split(" ");
+
+
+
+        if (inputTokens.length == pigTokens.length){
+            System.out.println();
+            for (int i=0;i<inputTokens.length;i++) {
+                System.out.println(ANSI_RED + inputTokens[i] + ANSI_YELLOW + "   --->   " + ANSI_GREEN + pigTokens[i] + "\n" + ANSI_RESET);
+            }
+        }
+
 
     }
 
@@ -75,15 +111,16 @@ public class PigLatin {
         System.out.println("Usage:");
         System.out.println("Just running the program will start a terminal interface where the user can introduce his sentence, and check the piggified result.");
         System.out.println("Running the program with the flag <-authors> will show information about the development team behind this project.");
+        System.out.println("Running the program with the flag <-verbose> will detail the translation process.");
 
     }
 
     private static void printAuthorsInfo(){
 
-        System.out.println(ANSI_PURPLE + "The bright minds behind this project:" + ANSI_RESET);
+        System.out.println(ANSI_CYAN + "The bright minds behind this project:" + ANSI_RESET);
         System.out.println(ANSI_YELLOW + "Amir Roozbeh" + ANSI_RESET);
         System.out.println(ANSI_RED + "Shahbaz Khader" + ANSI_RESET);
-        System.out.println(ANSI_CYAN + "Rui Oliveira" + ANSI_RESET);
+        System.out.println(ANSI_GREEN + "Rui Oliveira" + ANSI_RESET);
 
     }
 
